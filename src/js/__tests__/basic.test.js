cb4b9a9a995eb799d052f2cc67d936de383a78b6
Dima Shirkov <dimashirkov@list.ru>
2022-10-19 15:16:30 +0300

    eslint adjustment

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,33 +1,33 @@
-import attackResult, { matchers } from '../basic.js';
+import attackResult, { matchers } from '../basic';
 
 test('receiving damage', () => {
-    const healer = attackResult({name: 'Целитель', health: 50});
+  const healer = attackResult({ name: 'Целитель', health: 50 });
 
-    expect(healer).toBe('wounded');
+  expect(healer).toBe('wounded');
 });
 
 test('receiving critical damage', () => {
-    const woundedArcher = attackResult({name: 'Раненный лучник', health: 30});
+  const woundedArcher = attackResult({ name: 'Раненный лучник', health: 30 });
 
-    expect(woundedArcher).toBe('critical');
+  expect(woundedArcher).toBe('critical');
 });
 
 test('light blow', () => {
-    const destroyer = attackResult({name: 'Разрушитель', health: 100});
+  const destroyer = attackResult({ name: 'Разрушитель', health: 100 });
 
-    expect(destroyer).toBe('healthy');
+  expect(destroyer).toBe('healthy');
 });
 
 test('health ranking', () => {
-    const party = matchers([
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-    ]);
+  const party = matchers([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
 
-    expect(party).toEqual([
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-    ]);
+  expect(party).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,28 +1,28 @@
 export default function attackResult(characterParameters) {
-    const attack = 25;
-    let result;
+  const attack = 25;
+  let result;
 
-    Object.entries(characterParameters).forEach(([characteristic, value]) => {
-        if (characteristic === 'health') {
-            characterParameters.health = value - attack;
-            if (characterParameters.health < 15) {
-                result = 'critical';
-            } else if (characterParameters.health <= 50) {
-                result = 'wounded';
-            } else {
-                result = 'healthy';
-            }
-        }
-    })
+  Object.entries(characterParameters).forEach(([characteristic, value]) => {
+    if (characteristic === 'health') {
+      const characterHealth = value - attack;
+      // characterParameters.health = value - attack;
+      if (characterHealth < 15) {
+        result = 'critical';
+      } else if (characterHealth <= 50) {
+        result = 'wounded';
+      } else {
+        result = 'healthy';
+      }
+    }
+  });
 
-    return result;
+  return result;
 }
 
-
 export function matchers(groupMembers) {
-    function sortedByHealth(hp) {
-        return (a,b) => a[hp] < b[hp] ? 1 : -1;
-    }
+  function sortedByHealth(hp) {
+    return (a, b) => (a[hp] < b[hp] ? 1 : -1);
+  }
 
-    return groupMembers.sort(sortedByHealth('health'));
+  return groupMembers.sort(sortedByHealth('health'));
 }
